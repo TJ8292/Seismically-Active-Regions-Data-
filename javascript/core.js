ee9f7139ebd39076c7e4259a84220b98707008ae
Mogren <alexander.mogren@live.se>
2018-03-02 22:56:04 +0100

    Add files via upload

--- javascript/core.js
+++ javascript/core.js
@@ -3,13 +3,6 @@ var container = document.getElementById('popup');
 var content = document.getElementById('popup-content');
 var closer = document.getElementById('popup-closer');
 
-var popStyle = new ol.style.Style({
-stroke: new ol.style.Stroke({
-color: [0, 0, 255, 1.0],
-width: 1
-})
-});
-
 var popSelected = new ol.style.Style({
 stroke: new ol.style.Stroke({
 color: [0, 255, 255, 1.0],
@@ -34,15 +27,27 @@ width: 2
 })
 });
 
-var pointStyle = new ol.style.Style({
+var popStyle = new ol.style.Style({
 image: new ol.style.Circle({
 fill: new ol.style.Fill({
 color: 'red'
 }),
 stroke: new ol.style.Stroke({
-color: 'grey'
+color: 'black'
 }),
 radius: 5
+})
+});
+
+var popSelected = new ol.style.Style({
+image: new ol.style.Circle({
+fill: new ol.style.Fill({
+color: 'green'
+}),
+stroke: new ol.style.Stroke({
+color: 'black'
+}),
+radius: 8
 })
 });
 
@@ -79,11 +84,12 @@ width: 2
 var vectorSource = new ol.source.Vector();
 var vector = new ol.layer.Vector({
 source: vectorSource,
+style: popStyle
 
 });
 var vectorSource2 = new ol.source.Vector();
 var vector2 = new ol.layer.Vector({
-source: vectorSource2,
+source: vectorSource2
 
 });
 /**
@@ -120,42 +126,14 @@ radius: 16
 })
 });
 
-//point's definition and coordinates
-var pointFeature = new ol.Feature(
-new ol.geom.Point(ol.proj.transform([-99.4411447,38.495586],'EPSG:4326','EPSG:900913'))
-);
-
-//attaching style to a point
-pointFeature.setStyle(pointStyle);
-
-//creation of point layer
-var vectorPoint= new ol.layer.Vector({
-source: new ol.source.Vector({
-features: [pointFeature]
-})
-});
-
-/* Add layer of point features */
-var pointsLayer = new ol.layer.Vector({
-title: 'points',
-source : new ol.source.Vector({
-url : 'geojson/pop-us.geojson',
-format : new ol.format.GeoJSON()
-})
-});
-
-//creation of a new vector layer, which will store drawing of buffer
-var vectorBuffers= new ol.layer.Vector({
-source: new ol.source.Vector({})
-});
-
-
 var osm = new ol.layer.Tile({
-source: new ol.source.OSM()
+source: new ol.source.Stamen({
+	layer: 'terrain'
+})
 });
 
 var map = new ol.Map({
-layers: [osm, vector, vector2,vectorBuffers,vectorPoint],
+layers: [osm, vector, vector2],
 overlays: [overlay],
 target: document.getElementById('map'),
 view: new ol.View({
@@ -171,7 +149,6 @@ featureNS: 'usa',
 featurePrefix: 'usa',
 featureTypes: ['pop-us'],
 outputFormat: 'application/json'
-
 });
 
 fetch('http://localhost:8080/geoserver/wfs', {
@@ -210,16 +187,100 @@ var selectInteraction = new ol.interaction.Select({
 layers: function(layer) {
 return layer.get('selectable') == true;
 },
-style: [railwaysSelected, sweAdmSelected]
+style: [popSelected, sweAdmSelected]
 });
 
 vector.set('selectable', true);
 vector2.set('selectable', true);
 map.getInteractions().extend([selectInteraction]);
 
-selectInteraction.on('select', function(e) {
-var s = e.selected[0].getGeometry().getExtent();
-console.log(s);
+//selectInteraction.on('select', function(e) {
+//var s = e.selected[0].getGeometry().getExtent();
+//console.log(s);
+//}
+/**
+* Add a click handler to the map to render the popup.
+*/
+
+map.on('singleclick', function(evt) {
+var coordinate = evt.coordinate;
+var stringifyFunc = ol.coordinate.createStringXY(2);
+var out = stringifyFunc(coordinate);
+var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+  var lon = lonlat[0];
+  var lat = lonlat[1];
+
+  //point's definition and coordinates
+var pointFeature = new ol.Feature(
+new ol.geom.Point(ol.proj.transform([lon,lat],'EPSG:4326','EPSG:900913'))
+);
+
+//attaching style to a point
+pointFeature.setStyle(pointStyle);
+
+//creation of point layer
+var vectorPoint= new ol.layer.Vector({
+source: new ol.source.Vector({
+features: [pointFeature]
+})
+});
+
+//creation of a new vector layer, which will store drawing of buffer
+var vectorBuffers= new ol.layer.Vector({
+source: new ol.source.Vector({})
+});
+  
+var feature = map.forEachFeatureAtPixel(evt.pixel,
+function(feature, layer) {
+// do stuff here
+content.innerHTML = '<p>Information:</p><code>' +
+'<p>Cordinates: ' + lon + ' ' + lat + '</p>' + 'Id: ' + feature.getId() +
+'</code>';
+overlay.setPosition(coordinate);
+});
+    var x = document.getElementById("buf").checked;
+	var dist = document.getElementById("distance").value;
+	console.log(dist);
+    if(x){
+    bufferit(dist,pointFeature,vectorBuffers,vectorPoint);
+    }
+	
+	var y = document.getElementById("sel").checked;
+    if(y){
+    
+    }
+
+});
+
+
+//document.getElementById('bufferit').onclick = function (){
+//var bufferRadius = 1000000;
+//bufferit(bufferRadius)
+//};
+
+//function that uses point's geometry
+//and draws a polygon around it,
+//then the drawn features are added to the vectorBuffers
+function bufferit(radius,pointFeature,vectorBuffers,vectorPoint){
+map.addLayer(vectorPoint);
+map.addLayer(vectorBuffers);
+var poitnExtent = pointFeature.getGeometry().getExtent();
+//var poitnExtent = pointsLayer.getSource().getExtent();
+var bufferedExtent = new ol.extent.buffer(poitnExtent,radius);
+
+console.log(bufferedExtent);
+var bufferPolygon = new ol.geom.Polygon(
+[
+[[bufferedExtent[0],bufferedExtent[1]],
+[bufferedExtent[0],bufferedExtent[3]],
+[bufferedExtent[2],bufferedExtent[3]],
+[bufferedExtent[2],bufferedExtent[1]],
+[bufferedExtent[0],bufferedExtent[1]]]
+]
+);
+console.log("bufferPolygon",bufferPolygon);
+var bufferedFeature = new ol.Feature(bufferPolygon);
+vectorBuffers.getSource().addFeature(bufferedFeature);
 
 var featureRequest3 = new ol.format.WFS().writeGetFeature({
 srsName: 'EPSG:3857',
@@ -227,7 +288,7 @@ featureNS: 'usa',
 featurePrefix: 'usa',
 featureTypes: ['pop-us'],
 outputFormat: 'application/json',
-filter: new ol.format.filter.Bbox('the_geom', s, 'EPSG:3857')
+filter: new ol.format.filter.Bbox('the_geom', bufferedExtent, 'EPSG:3857')
 });
 
 // then post the request and add the received features to a layer
@@ -244,51 +305,8 @@ vectorSource.addFeatures(features3);
 map.addLayer(vector);
 extent_swe = vectorSource.getExtent();
 });
-});
-/**
-* Add a click handler to the map to render the popup.
-*/
-map.on('singleclick', function(evt) {
-var coordinate = evt.coordinate;
-var stringifyFunc = ol.coordinate.createStringXY(2);
-var out = stringifyFunc(coordinate);
-var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
-  var lon = lonlat[0];
-  var lat = lonlat[1];
-var feature = map.forEachFeatureAtPixel(evt.pixel,
-function(feature, layer) {
-// do stuff here
-content.innerHTML = '<p>Information:</p><code>' +
-'<p>Cordinates: ' + lon + ' ' + lat + '</p>' + 'Id: ' + feature.getId() +
-'</code>';
-overlay.setPosition(coordinate);
-});
-});
 
+//map.addLayer(vectorPoint);
+//map.addLayer(vectorBuffers);
 
-document.getElementById('bufferit').onclick = function (){
-var bufferRadius = 1000000;
-bufferit(bufferRadius)
-};
-
-//function that uses point's geometry
-//and draws a polygon around it,
-//then the drawn features are added to the vectorBuffers
-function bufferit(radius){
-var poitnExtent = pointFeature.getGeometry().getExtent();
-//var poitnExtent = pointsLayer.getSource().getExtent();
-var bufferedExtent = new ol.extent.buffer(poitnExtent,radius);
-console.log(bufferedExtent);
-var bufferPolygon = new ol.geom.Polygon(
-[
-[[bufferedExtent[0],bufferedExtent[1]],
-[bufferedExtent[0],bufferedExtent[3]],
-[bufferedExtent[2],bufferedExtent[3]],
-[bufferedExtent[2],bufferedExtent[1]],
-[bufferedExtent[0],bufferedExtent[1]]]
-]
-);
-console.log("bufferPolygon",bufferPolygon);
-var bufferedFeature = new ol.Feature(bufferPolygon);
-vectorBuffers.getSource().addFeature(bufferedFeature);
-}
+ }
